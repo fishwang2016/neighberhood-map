a91c7808ed1a1f2dba78d64c03ddd2ddd7d9edc8
fishwang2016 <zhenyu2004@hotmail.com>
2016-06-05 15:30:56 +0800

    add styles

--- js/app.js
+++ js/app.js
@@ -1,36 +1,46 @@
+/*
+
+* This is a Udacity Front End Nano Degree Project
+* Author: Zhenyu Wang
+* 2016.06.05
+
+*/
+
+
 function ModelView(){
 
             var self = this;
-
-
             self.list_filter= ko.observable();
             self.textReceived = ko.observable();
-            self.locations = ko.observableArray([{placeName:"Center",show:ko.observable(true)},
-            	 {placeName:"Timsa",show:ko.observable(false)},{placeName:"Mongko",show:ko.observable(true)},
-            	 {placeName:"Shatian", show:ko.observable(false)}]);
+            self.locations = ko.observableArray([{placeName:"Central, Hong Kong",show:ko.observable(true)},
+            	 {placeName:"Hong Kong University",show:ko.observable(false)},
+                   {placeName:"Lan Kwai Fong",show:ko.observable(true)},
+            	 {placeName:"Sun Yat Sen Memorial Park", show:ko.observable(true)},
+                   {placeName:"International Finance Center", show:ko.observable(true)},
+                   {placeName:"Victoria Peak", show:ko.observable(true)},
+                   {placeName:"HIGH WEST", show:ko.observable(true)},
+                   {placeName:"WAN CHAI", show:ko.observable(true)},
+                   {placeName:"Hong kong Jockey Club", show:ko.observable(true)},
+                   {placeName:"Lung Fu SHAN", show:ko.observable(true)}
+
+                   ]);
 
             self.filter = function(data){
 
-            	console.log(data.textReceived());
+            	//console.log(data.textReceived());
             	var pattern = data.textReceived();
             	re = new  RegExp( pattern ,"i") ;
             	self.locations().forEach(function(location){
-
-            		console.log(location.placeName.match(re)===null);
-
+            		//console.log(location.placeName.match(re)===null);
             		if (location.placeName.match(re)===null){
-
-            			 location.show(ko.observable(false));// not  location.show = ko.observable(true
-                          console.log(location.show + "set to false")
+            			 location.show(false);// not  location.show = ko.observable(true
+                          
             		}else{
-
-            			location.show(ko.observable(true));// not  location.show = ko.observable(true
-                                    // it looks this function doesn't work well.
+                              // This is updating instance value.
+            			location.show(true);// not  location.show = ko.observable(true)                   
             		}
 
             		});
-
-
             	};
 
             }
