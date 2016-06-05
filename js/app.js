cd9d98929f47d19455e7d3747541d358320d677d
fishwang2016 <fish@taifanmagellan.com>
2016-06-05 08:13:05 +0800

    adding filter

--- js/app.js
+++ js/app.js
@@ -1,14 +1,17 @@
- function ModelView(){
+function ModelView(){
 
- 	var self = this;
+            var self = this;
+            console.log("init");
 
- 	self.filter = ko.observable()
- 	self.list_filter= ko.observable();
+            self.filter = ko.observable()
+            self.list_filter= ko.observable();
 
- 	self.location = ko.observableArray([]);
+            self.locations = ko.observableArray([{placeName:"Center"},{placeName:"Timsa"},{placeName:"Mongko"},{placeName:"Shatian"}]);
+            console.log(self.locations);
 
+          }
 
- }
+ko.applyBindings(new ModelView());
 
 
  // input change -> filter  on slected -> marker change
